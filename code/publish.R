67207edcf87761c6f1e71789854142af85b473c5
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-07-08 10:28:24 -0300

    altera estrutura de função para script direto

--- code/publish.R
+++ code/publish.R
@@ -1,9 +1,52 @@
+# title: Repositório Brasileiro Livre para Dados Abertos do Solo
+# subtitle: Publicação de conjuntos de dados
+# autor: Alessandro Samuel-Rosa
 
-x <- openxlsx::read.xlsx(
-  xlsxFile = '/home/alessandrorosa/ownCloud/febr-repo/processamento/ctb0001/2020-04-21-ctb0001.xlsx', 
-  sheet = 'camada')
+# TODO ----
+# 1. Adicionar campo dados_idioma
+# 2. Substituir campo autor_nome_email por dados_autor
+# 3. Substituir campo dados_referencia por dados_publicacao
+
+# variables ----
+ctb <- ""
+
+# main ########################################################################################################
+
+# caminhos dos diretórios de dados
+processamento <- path.expand('~/ownCloud/febr-repo/processamento/')
+publico <- path.expand('~/ownCloud/febr-repo/publico/')
+
+# identificar workbook com dados a serem publicados
+# requisito: arquivo XLSX com a data de hoje
+xlsxFile <- paste(processamento, ctb, '/', Sys.Date(), '-', ctb, '.xlsx', sep = '')
+
+# processar tabela identificacao
+identificacao <- openxlsx::read.xlsx(xlsxFile, sheet = 'identificacao')
+consistencia <- identificacao[identificacao$campo == 'dados_consistencia', 'valor']
+file <- paste(publico, ctb, '/', ctb, '-identificacao.csv', sep = '')
+if(!dir.exists(dirname(file))) {
+  dir.create(dirname(file))
+}
+write.table(x = identificacao, file = file, sep = ';', dec = ',', row.names = FALSE)
+
+# processar tabela versionamento
+file <- paste(publico, ctb, '/', ctb, '-versionamento.csv', sep = '')
 write.table(
-  x = x,
-  file = '/home/alessandrorosa/ownCloud/febr-repo/processamento/ctb0001/ctb0001-camada.csv', 
-  sep = ';', dec = ',', row.names = FALSE)
+  x = openxlsx::read.xlsx(xlsxFile, sheet = 'versionamento'), 
+  file = file, sep = ';', dec = ',', row.names = FALSE)
 
+# processar tabelas metadado, observacao e camada
+# requisito: consistência estrutural
+if (consistencia == 'consistência estrutural') {
+  tab <- c('metadado', 'observacao', 'camada')
+  for (i in 1:length(tab)) {
+    x <- openxlsx::read.xlsx(xlsxFile, sheet = tab[i])
+    x <- x[colnames(x) != '']
+    file <- paste(publico, ctb, '/', ctb, '-', tab[i], '.csv', sep = '')
+    write.table(x = x, file = file, sep = ';', dec = ',', row.names = FALSE)
+  }
+}
+
+# copiar workbook para diretório público
+cmd <- paste('cp', xlsxFile, paste(publico, ctb, '/', ctb, '.xlsx', sep = ''))
+system(cmd)
