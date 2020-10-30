3582527485364529e2889b8cf05468faffd844b0
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2020-10-30 19:04:14 -0300

    various improvements

--- code/publish.R
+++ code/publish.R
@@ -12,56 +12,83 @@
 # main ########################################################################################################
 rm(list = ls())
 
-# conjunto de dados a ser publicado
-ctb <- "ctb0025"
-
 # caminhos dos diretórios de dados
-processamento <- path.expand('~/ownCloud/febr-repo/processamento/')
-publico <- path.expand('~/ownCloud/febr-repo/publico/')
+processamento <- path.expand('~/ownCloud/febr-repo/processamento')
+publico <- path.expand('~/ownCloud/febr-repo/publico')
 
-# identificar workbook com dados a serem publicados
-# requisito: arquivo XLSX com a data de hoje
-xlsxFile <- paste(processamento, ctb, '/', Sys.Date(), '-', ctb, '.xlsx', sep = '')
+# conjunto de dados a ser publicado
+# ctb <- list.dirs(processamento, recursive = FALSE, full.names = FALSE)
+ctb <- "ctb0020"
 
-# Processar tabela 'identificacao'
-# Campos de identificação são obtidos da planilha padrão armazenada no Google Drive
-# Campos faltantes são inseridos. Em seguida os campos são reordenados conforme o padrão
-# (esse processo também é realizado em make-index.R)
-id_campo <- 
-  paste0('https://docs.google.com/spreadsheets/d/', '1rXIiT1zSYhFegSdAvE0yJX16q-bvXVNpYIYdd5YgjhI', 
-         '/export?format=csv&gid=', '1085102806')
-id_campo <- read.csv(id_campo, header = TRUE, stringsAsFactors = FALSE)
-identificacao <- openxlsx::read.xlsx(xlsxFile, sheet = 'identificacao')
-identificacao$campo[identificacao$campo == 'autor_nome_email'] <- 'dados_autor'
-identificacao$campo[identificacao$campo == 'dados_referencia'] <- 'dados_publicacao'
-identificacao <- merge(identificacao, id_campo['campo'], all.y = TRUE)
-identificacao <- identificacao[match(id_campo$campo, identificacao$campo), ]
-file <- paste0(publico, ctb, '/', ctb, '-identificacao.txt', sep = '')
-if(!dir.exists(dirname(file))) {
-  dir.create(dirname(file))
-}
-write.table(x = identificacao, file = file, sep = '\t', dec = ',', row.names = FALSE)
-
-# processar tabela 'versionamento'
-file <- paste0(publico, ctb, '/', ctb, '-versionamento.txt')
-write.table(
-  x = openxlsx::read.xlsx(xlsxFile, sheet = 'versionamento'), 
-  file = file, sep = '\t', dec = ',', row.names = FALSE)
-
-# processar tabelas 'metadado', 'observacao' e 'camada'
-# requisito: consistência estrutural
-consistencia <- identificacao[identificacao$campo == 'dados_consistencia', 'valor']
-if (consistencia == 'consistência estrutural') {
-  tab <- c('metadado', 'observacao', 'camada')
-  for (i in 1:length(tab)) {
-    # i <- 1
-    x <- openxlsx::read.xlsx(xlsxFile, sheet = tab[i])
-    x <- x[colnames(x) != '']
-    file <- paste0(publico, ctb, '/', ctb, '-', tab[i], '.txt')
-    write.table(x = x, file = file, sep = '\t', dec = ',', row.names = FALSE)
+for (i in seq_along(ctb)) {
+  # i <- 1
+  # identificar workbook com dados a serem publicados
+  # requisito: arquivo XLSX com a data de hoje
+  # xlsxFile <- list.files(path = paste0(processamento, "/", ctb[i]), pattern = "xlsx", full.names = TRUE)
+  # xlsxFile <- xlsxFile[length(xlsxFile)]
+  # xlsxFile <- paste0(processamento, "/", ctb, '/', Sys.Date(), '-', ctb, '.xlsx')
+  xlsxFile <- list.files(
+    path = paste0(processamento, "/", ctb[i]), 
+    pattern = paste0(Sys.Date()), 
+    full.names = TRUE)
+  
+  # Carregar workbook
+  workbook <- openxlsx::loadWorkbook(xlsxFile)
+  
+  # Processar tabela 'identificacao'
+  # Campos de identificação são obtidos da planilha padrão armazenada no Google Drive
+  # Campos faltantes são inseridos. Em seguida os campos são reordenados conforme o padrão
+  # (esse processo também é realizado em make-index.R)
+  id_campo <- 
+    paste0('https://docs.google.com/spreadsheets/d/', '1rXIiT1zSYhFegSdAvE0yJX16q-bvXVNpYIYdd5YgjhI', 
+           '/export?format=csv&gid=', '1085102806')
+  id_campo <- read.csv(id_campo, header = TRUE, stringsAsFactors = FALSE)
+  identificacao <- openxlsx::read.xlsx(workbook, sheet = 'identificacao')
+  rownames(identificacao) <- identificacao$campo
+  identificacao["autor_nome_email", "campo"] <- "dados_autor"
+  identificacao["dados_referencia", "campo"] <- "dados_publicacao"
+  identificacao <- merge(x = identificacao, y = id_campo['campo'], all.y = TRUE)
+  identificacao <- identificacao[match(id_campo$campo, identificacao$campo), ]
+  rownames(identificacao) <- identificacao$campo
+  if (as.integer(sub("ctb", "", ctb[i])) > 100) {
+    identificacao["dados_licenca", "valor"] <- "CC BY-NC 4.0 Atribuição-NãoComercial"
   }
+  file <- paste0(publico, "/", ctb[i], '/', ctb[i], '-identificacao.txt', sep = '')
+  if(!dir.exists(dirname(file))) {
+    dir.create(dirname(file))
+  }
+  write.table(x = identificacao, file = file, sep = '\t', dec = ',', row.names = FALSE)
+  
+  # processar tabela 'versionamento'
+  file <- paste0(publico, '/', ctb[i], '/', ctb[i], '-versionamento.txt')
+  versionamento <- openxlsx::read.xlsx(workbook, sheet = 'versionamento')
+  write.table(x = versionamento, file = file, sep = '\t', dec = ',', row.names = FALSE)
+  
+  # Verificar se conjunto de dados está sob embargo
+  embargado <- grepl("embargado", xlsxFile)
+  if (embargado) {
+    tab <- c('metadado')
+  } else {
+    tab <- c('metadado', 'observacao', 'camada')
+  }
+  # processar tabelas 'metadado', 'observacao' e 'camada'
+  # requisito: consistência estrutural
+  consistencia <- identificacao[identificacao$campo == 'dados_consistencia', 'valor']
+  if (consistencia == 'consistência estrutural') {
+    for (j in 1:length(tab)) {
+      x <- openxlsx::read.xlsx(workbook, sheet = tab[j])
+      x <- x[colnames(x) != '']
+      file <- paste0(publico, '/', ctb[i], '/', ctb[i], '-', tab[j], '.txt')
+      write.table(x = x, file = file, sep = '\t', dec = ',', row.names = FALSE)
+    }
+  }
+  
+  if (!embargado) {
+    # copiar workbook para diretório público
+    cmd <- paste('cp', xlsxFile, paste0(publico, '/', ctb[i], '/', ctb[i], '.xlsx'))
+    system(cmd)
+  }
+  
+  # Atualizar catálogo de conjuntos de dados
+  source("code/make-index.R")
 }
-
-# copiar workbook para diretório público
-cmd <- paste('cp', xlsxFile, paste0(publico, ctb, '/', ctb, '.xlsx'))
-system(cmd)
