b50b108e14a4c0102377af28351c1559ef48505e
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2022-01-21 18:53:05 -0300

    update documentation

--- src/publish.R
+++ src/publish.R
@@ -1,11 +1,11 @@
-# title: Repositório Brasileiro Livre para Dados Abertos do Solo
-# subtitle: Publicação de conjuntos de dados
-# autor: Alessandro Samuel-Rosa
+# title: Publicação de Dados do Solo
+# subtitle: Repositório de Dados do Solo Brasileiro
+# author: Alessandro Samuel-Rosa
 # summary:
 #   * Publicar conjunto de dados processado no dia de hoje, criando um arquivo de texto TXT para
 #     cada uma das planilhas existentes, bem como uma cópia do arquivo XLSX.
 #   * Os arquivos gerados são copiados/salvos no respectivo diretório do conjunto de dados no
-#     diretório 'public'.
+#     diretório 'publico'.
 #   * Para conjuntos de dados sem consistência documental, ou seja, em que faltam todos ou alguns
 #     dos campos obrigatórios das tabelas observacao e camada, faz-se a adição dos mesmos a fim de
 #     permitir o descarregamento dos arquivos via pacote febr sem erro.
@@ -16,7 +16,7 @@ rm(list = ls())
 processamento <- path.expand('~/ownCloud/febr-repo/processamento')
 publico <- path.expand('~/ownCloud/febr-repo/publico')
 
-# conjunto de dados a ser publicado
+# identificação dos dados a serem publicados
 # ctb <- list.dirs(processamento, recursive = FALSE, full.names = FALSE)
 ctb <- "ctb0025"
 
@@ -28,22 +28,21 @@ for (i in seq_along(ctb)) {
   # xlsxFile <- xlsxFile[length(xlsxFile)]
   # xlsxFile <- paste0(processamento, "/", ctb, '/', Sys.Date(), '-', ctb, '.xlsx')
   xlsxFile <- list.files(
-    path = paste0(processamento, "/", ctb[i]), 
-    pattern = paste0(Sys.Date()), 
+    path = paste0(processamento, "/", ctb[i]),
+    pattern = paste0(Sys.Date()),
     full.names = TRUE)
-  
   # Carregar workbook
   workbook <- openxlsx::loadWorkbook(file = xlsxFile)
-  
   # Processar tabela 'identificacao'
   # Campos de identificação são obtidos da planilha padrão armazenada no Google Drive
   # Campos faltantes são inseridos. Em seguida os campos são reordenados conforme o padrão
   # (esse processo também é realizado em make-index.R)
-  id_campo <- 
-    paste0('https://docs.google.com/spreadsheets/d/', '1rXIiT1zSYhFegSdAvE0yJX16q-bvXVNpYIYdd5YgjhI', 
-           '/export?format=csv&gid=', '1085102806')
+  id_campo <-
+    paste0("https://docs.google.com/spreadsheets/d/",
+      "1rXIiT1zSYhFegSdAvE0yJX16q-bvXVNpYIYdd5YgjhI",
+      "/export?format=csv&gid=", "1085102806")
   id_campo <- read.csv(id_campo, header = TRUE, stringsAsFactors = FALSE)
-  identificacao <- openxlsx::read.xlsx(workbook, sheet = 'identificacao')
+  identificacao <- openxlsx::read.xlsx(workbook, sheet = "identificacao")
   rownames(identificacao) <- identificacao$campo
   identificacao["autor_nome_email", "campo"] <- "dados_autor"
   identificacao["dados_referencia", "campo"] <- "dados_publicacao"
@@ -53,17 +52,16 @@ for (i in seq_along(ctb)) {
   if (as.integer(sub("ctb", "", ctb[i])) > 100) {
     identificacao["dados_licenca", "valor"] <- "CC BY-NC 4.0 Atribuição-NãoComercial"
   }
-  file <- paste0(publico, "/", ctb[i], '/', ctb[i], '-identificacao.txt', sep = '')
-  if(!dir.exists(dirname(file))) {
+  file <- paste0(publico, "/", ctb[i], "/", ctb[i], "-identificacao.txt", sep = "")
+  if (!dir.exists(dirname(file))) {
     dir.create(dirname(file))
   }
-  write.table(x = identificacao, file = file, sep = '\t', dec = ',', row.names = FALSE)
-  
+  write.table(x = identificacao, file = file, sep = "\t", dec = ",", row.names = FALSE)
+  #
   # processar tabela 'versionamento'
   file <- paste0(publico, '/', ctb[i], '/', ctb[i], '-versionamento.txt')
   versionamento <- openxlsx::read.xlsx(workbook, sheet = 'versionamento')
   write.table(x = versionamento, file = file, sep = '\t', dec = ',', row.names = FALSE)
-  
   # Verificar se conjunto de dados está sob embargo
   embargado <- grepl("embargado", xlsxFile)
   if (embargado) {
@@ -77,18 +75,16 @@ for (i in seq_along(ctb)) {
   if (consistencia == 'consistência estrutural') {
     for (j in 1:length(tab)) {
       x <- openxlsx::read.xlsx(workbook, sheet = tab[j])
-      x <- x[colnames(x) != '']
-      file <- paste0(publico, '/', ctb[i], '/', ctb[i], '-', tab[j], '.txt')
-      write.table(x = x, file = file, sep = '\t', dec = ',', row.names = FALSE)
+      x <- x[colnames(x) != ""]
+      file <- paste0(publico, "/", ctb[i], "/", ctb[i], "-", tab[j], ".txt")
+      write.table(x = x, file = file, sep = "\t", dec = ",", row.names = FALSE)
     }
   }
-  
   if (!embargado) {
     # copiar workbook para diretório público
-    cmd <- paste('cp', xlsxFile, paste0(publico, '/', ctb[i], '/', ctb[i], '.xlsx'))
+    cmd <- paste("cp", xlsxFile, paste0(publico, "/", ctb[i], "/", ctb[i], ".xlsx"))
     system(cmd)
   }
-
   # Atualizar catálogo de conjuntos de dados
   source("src/make-index.R")
 }
