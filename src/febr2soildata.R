de44e3ddfe10eac7202678aee0da24ab30fc7f10
Alessandro Samuel-Rosa <alessandrosamuelrosa@gmail.com>
2024-06-27 15:28:49 -0300

    Adds description to the script

--- src/febr2soildata.R
+++ src/febr2soildata.R
@@ -1,9 +1,19 @@
-# # Load and publish datasets to the SoilData Dataverse instance
+# title: Load and publish datasets to the SoilData Dataverse instance
+# author: Alessandro Samuel-Rosa
+# description: This script loads datasets from the FEBR repository and publishes them to the
+# SoilData Dataverse instance. The script reads the FEBR index file, which contains metadata
+# for each dataset, and uses a template JSON file to create the metadata for the datasets.
+# The script then iterates over the datasets, updating the metadata in the JSON file and
+# publishing the dataset to the SoilData Dataverse instance using the Dataverse API.
+# note: The script DOES NOT publish the actual data files, only the metadata.
 
-# # Install and load required packages
-# if (!require("dataverse")) {
-#   install.packages("dataverse")
-# }
+# Install and load required packages
+if (!require("dataverse")) {
+  install.packages("dataverse")
+}
+if (!require("RJSONIO")) {
+  install.packages("RJSONIO")
+}
 
 # # Set environment variable DATAVERSE_SERVER
 # Sys.setenv("DATAVERSE_SERVER" = "https://solo.mapbiomas.org")
@@ -18,21 +28,24 @@
 # )
 # str(dnos$license)
 
-
-# Install and load required packages
-if (!require("RJSONIO")) {
-  install.packages("RJSONIO")
-}
-
 # Read FEBR index
 index <- data.table::fread("/home/alessandro/ownCloud/febr-repo/publico/febr-indice.txt")
 index <- index[-3, ] # remove datasets already published
 index[, dados_licenca := gsub(" Atribuição-NãoComercial", "", index[, dados_licenca])]
 index[, dados_licenca := gsub(" Atribuição", "", index[, dados_licenca])]
 index[, dados_licenca := gsub("-CompartilhaIgual", "", index[, dados_licenca])]
-index[dados_licenca == "CC BY 4.0", dados_licenca_uri := "http://creativecommons.org/licenses/by/4.0"]
-index[dados_licenca == "CC BY-NC 4.0", dados_licenca_uri := "http://creativecommons.org/licenses/by-nc/4.0/"]
-index[dados_licenca == "CC BY-NC-SA 4.0", dados_licenca_uri := "http://creativecommons.org/licenses/by-nc-sa/4.0/"]
+index[
+  dados_licenca == "CC BY 4.0",
+  dados_licenca_uri := "http://creativecommons.org/licenses/by/4.0"
+]
+index[
+  dados_licenca == "CC BY-NC 4.0",
+  dados_licenca_uri := "http://creativecommons.org/licenses/by-nc/4.0/"
+]
+index[
+  dados_licenca == "CC BY-NC-SA 4.0",
+  dados_licenca_uri := "http://creativecommons.org/licenses/by-nc-sa/4.0/"
+]
 
 # Read template JSON file with metadata
 meta <- RJSONIO::fromJSON("febr-output/src/dataset-create-new-all-default-fields.json")
@@ -44,23 +57,33 @@ i <- i-1
   meta$datasetVersion$license[2] <- index[i, dados_licenca_uri]
   meta$datasetVersion$metadataBlocks$citation$fields[[1]]$value <- index[i, dados_titulo]
   meta$datasetVersion$metadataBlocks$citation$fields[[2]]$value <- index[i, dados_acesso]
-  meta$datasetVersion$metadataBlocks$citation$fields[[3]]$value[[1]]$otherIdValue$value <- index[i, dados_id]
+  meta$datasetVersion$metadataBlocks$citation$fields[[3]]$value[[1]]$otherIdValue$value <-
+    index[i, dados_id]
   authors <- strsplit(index[i, dados_autor], "; ")[[1]]
   meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value <-
     meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[rep(1, length(authors))]
   for (k in seq_along(authors)) {
-    meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorName$value <- authors[k]
+    meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorName$value <-
+      authors[k]
     if (k == 1) {
       if (is.na(index[i, organizacao_nome])) {
         index[i, organizacao_nome := ""]
       }
       index[i, organizacao_nome := gsub(" (EMBRAPA)$", "", index[i, organizacao_nome])]
-      index[i, organizacao_nome := gsub("^Embrapa$", "Empresa Brasileira de Pesquisa Agropecuária", index[i, organizacao_nome], perl = TRUE)]
-      index[i, organizacao_nome := gsub("EMBRAPA SOLOS", "Embrapa Solos", index[i, organizacao_nome])]
+      index[i, organizacao_nome := gsub("^Embrapa$", "Empresa Brasileira de Pesquisa Agropecuária",
+        index[i, organizacao_nome],
+        perl = TRUE
+      )]
+      index[i, organizacao_nome := gsub(
+        "EMBRAPA SOLOS", "Embrapa Solos",
+        index[i, organizacao_nome]
+      )]
       index[i, organizacao_nome := gsub(" (IBGE)$", "", index[i, organizacao_nome])]
-      meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorAffiliation$value <- index[i, organizacao_nome]
+      meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorAffiliation$value <-
+        index[i, organizacao_nome]
     } else {
-      meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorAffiliation$value <- ""
+      meta$datasetVersion$metadataBlocks$citation$fields[[4]]$value[[k]]$authorAffiliation$value <-
+        ""
     }
   }
   # datasetContact
@@ -69,7 +92,8 @@ i <- i-1
   if (is.na(index[i, dados_descricao])) {
     index[i, dados_descricao := index[i, dados_titulo]]
   }
-  meta$datasetVersion$metadataBlocks$citation$fields[[6]]$value[[1]]$dsDescriptionValue$value <- index[i, dados_descricao]
+  meta$datasetVersion$metadataBlocks$citation$fields[[6]]$value[[1]]$dsDescriptionValue$value <-
+    index[i, dados_descricao]
   # subject
   # meta$datasetVersion$metadataBlocks$citation$fields[[7]]
   # keyword
@@ -80,13 +104,15 @@ i <- i-1
   meta$datasetVersion$metadataBlocks$citation$fields[[8]]$value <-
     meta$datasetVersion$metadataBlocks$citation$fields[[8]]$value[rep(1, length(keywords))]
   for (k in seq_along(keywords)) {
-    meta$datasetVersion$metadataBlocks$citation$fields[[8]]$value[[k]]$keywordValue$value <- keywords[k]
+    meta$datasetVersion$metadataBlocks$citation$fields[[8]]$value[[k]]$keywordValue$value <-
+      keywords[k]
   }
   # topicClassification
   if (is.na(index[i, area_conhecimento])) {
     index[i, area_conhecimento := ""]
   }
-  meta$datasetVersion$metadataBlocks$citation$fields[[9]]$value[[1]]$topicClassValue$value <- index[i, area_conhecimento]
+  meta$datasetVersion$metadataBlocks$citation$fields[[9]]$value[[1]]$topicClassValue$value <-
+    index[i, area_conhecimento]
   # publication
   publication <- strsplit(index[i, dados_publicacao], "; ")[[1]]
   if (is.na(publication[1])) {
@@ -95,7 +121,8 @@ i <- i-1
   meta$datasetVersion$metadataBlocks$citation$fields[[10]]$value <-
     meta$datasetVersion$metadataBlocks$citation$fields[[10]]$value[rep(1, length(publication))]
   for (k in seq_along(publication)) {
-    meta$datasetVersion$metadataBlocks$citation$fields[[10]]$value[[k]]$publicationCitation$value <- publication[k]
+    meta$datasetVersion$metadataBlocks$citation$fields[[10]]$value[[k]]$publicationCitation$value <-
+      publication[k]
   }
   # language
   # meta$datasetVersion$metadataBlocks$citation$fields[[11]] 
@@ -104,7 +131,8 @@ i <- i-1
   if (is.na(index[i, fonte_financeira])) {
     index[i, fonte_financeira := ""]
   }
-  meta$datasetVersion$metadataBlocks$citation$fields[[12]]$value[[1]]$grantNumberAgency$value <- index[i, fonte_financeira]
+  meta$datasetVersion$metadataBlocks$citation$fields[[12]]$value[[1]]$grantNumberAgency$value <-
+    index[i, fonte_financeira]
   
   # depositor
   # meta$datasetVersion$metadataBlocks$citation$fields[[13]]
@@ -113,7 +141,8 @@ i <- i-1
   if (is.na(index[i, extensao_espacial])) {
     index[i, extensao_espacial := ""]
   }
-  meta$datasetVersion$metadataBlocks$geospatial$fields[[1]]$value[[1]]$otherGeographicCoverage$value <- index[i, extensao_espacial]
+  meta$datasetVersion$metadataBlocks$geospatial$fields[[1]]$value[[1]]$otherGeographicCoverage$value <-
+    index[i, extensao_espacial]
 
   # write JSON file with metadata
   writeLines(RJSONIO::toJSON(meta), paste0("febr-output/res/", index[i, dados_id], ".json"))
